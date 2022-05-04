abc5ecb3abca0356dd30a3604271ed30b1afa09c
Anoopanu9633 <103813506+Anoopanu9633@users.noreply.github.com>
2022-05-04 16:03:48 +0530

    Update and rename simplecalculator.java to calculator.java
    this is the new edited file

--- calculator.java
+++ calculator.java
@@ -1,4 +1,4 @@
-package simplecalculator;
+package calculator;
 
 //Java program for simple calculator
 
@@ -7,7 +7,7 @@ import java.lang.*;
 import java.lang.Math;
 import java.util.Scanner;
 
-public class calcu {
+public class calculator {
 
 	public static void main(String[] args)
 	{
